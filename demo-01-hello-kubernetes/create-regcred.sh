384ec0b2d590befba2c8718b020e43b93a709ca8
root <root@admin.lab01.one>
2021-10-22 14:22:45 +1100

    update create tls cert script

---
+++ demo-01-hello-kubernetes/create-regcred.sh
@@ -0,0 +1,5 @@
+kubectl create secret generic regcred \
+--from-file=.dockerconfigjson=/root/.docker/config.json \
+--type=kubernetes.io/dockerconfigjson
+kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
+
