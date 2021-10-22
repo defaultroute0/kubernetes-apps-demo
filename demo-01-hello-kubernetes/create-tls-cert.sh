2b40e117a6b1766d189197ecc6be78e8ddbda4c1
root <root@admin.lab01.one>
2021-10-22 14:47:41 +1100

    update amko

--- demo-01-hello-kubernetes/create-tls-cert.sh
+++ demo-01-hello-kubernetes/create-tls-cert.sh
@@ -3,3 +3,5 @@ openssl req -x509 -nodes -days 365 \
 -out hello-ingress-tls.crt \
 -keyout hello-ingress-tls.key \
 -subj "/CN=hello.lb.lab01.one/O=hello-ingress-tls"
+kubectl create secret tls hello-ingress-tls --key hello-ingress-tls.key --cert hello-ingress-tls.crt
+
