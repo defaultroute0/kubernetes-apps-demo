2935bb135d733d3a3e7f51c045f8cc1d03972a26
root <root@admin.lab01.one>
2021-10-27 14:46:52 +1100

    update readme for dvwa demo

--- lb.lab01.one/demo-03-dvwa-app/cleanup.sh
+++ lb.lab01.one/demo-03-dvwa-app/cleanup.sh
@@ -3,3 +3,4 @@ kubectl delete -f dvwa-hostrule.yaml
 rm -rf dvwa-ingress-tls.key
 rm -rf dvwa-ingress-tls.crt
 kubectl delete secret dvwa-ingress-tls --namespace dvwa-apps
+kubectl delete ns dvwa-apps
