6c2865d2a77e681a9795e1b7712c2f0773a9915d
Ryan Douglas <67136481+defaultroute0@users.noreply.github.com>
2022-08-03 12:24:50 +1000

    Update create-regcred.sh

--- demo-01-hello-kubernetes/create-regcred.sh
+++ demo-01-hello-kubernetes/create-regcred.sh
@@ -1,6 +1,15 @@
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "/home/$USER/.docker" -R
 docker login
-kubectl create secret generic regcred \
---from-file=.dockerconfigjson=/root/.docker/config.json \
+kubectl create secret generic regcred -n hello \
+--from-file=.dockerconfigjson=/home/rdouglas/.docker/config.json \
 --type=kubernetes.io/dockerconfigjson
-kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
+kubectl patch serviceaccount default -n hello -p '{"imagePullSecrets": [{"name": "regcred"}]}'
+
+#orig
+#docker login
+#kubectl create secret generic regcred \
+#--from-file=.dockerconfigjson=/root/.docker/config.json \
+#--type=kubernetes.io/dockerconfigjson
+#kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
 
