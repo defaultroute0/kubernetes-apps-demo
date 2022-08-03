6cffeb58c36a23f3cb0c341b5bebf4df4e17242a
Ryan Douglas <67136481+defaultroute0@users.noreply.github.com>
2022-08-03 12:28:34 +1000

    Update create-tls-cert.sh

--- demo-01-hello-kubernetes/create-tls-cert.sh
+++ demo-01-hello-kubernetes/create-tls-cert.sh
@@ -1,7 +1,13 @@
-openssl req -x509 -nodes -days 365 \
+openssl req -x509 -nodes -days 900 \
 -newkey rsa:2048 \
--out hello-ingress-tls.crt \
--keyout hello-ingress-tls.key \
--subj "/CN=hello.lb.lab01.one/O=hello-ingress-tls"
-kubectl create secret tls hello-ingress-tls --key hello-ingress-tls.key --cert hello-ingress-tls.crt
-
+-out hello-ingress-tls-sitea.crt \
+-keyout hello-ingress-tls-sitea.key \
+-subj "/CN=hellositea.vcnlab01.eng.vmware.com/O=hello-ingress-tls-sitea"
+kubectl create secret tls hello-ingress-tls-sitea --key hello-ingress-tls-sitea.key --cert hello-ingress-tls-sitea.crt -n hello
+#
+openssl req -x509 -nodes -days 900 \
+-newkey rsa:2048 \
+-out hello-ingress-tls-siteb.crt \
+-keyout hello-ingress-tls-siteb.key \
+-subj "/CN=hellositeb.vcnlab01.eng.vmware.com/O=hello-ingress-tls-siteb"
+kubectl create secret tls hello-ingress-tls-siteb --key hello-ingress-tls-siteb.key --cert hello-ingress-tls-siteb.crt -n hello
